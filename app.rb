cd8f26f673129b0d1125519c4fe66f0708575fd0
renburnett <renardburnett@gmail.com>
2019-09-18 11:14:01 -0700

    created puppies!

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+        erb :index
+    end
+
+    #send user to puppy creation page
+    get '/new' do
+        erb :create_puppy
+    end
+
+    #create puppy and send user to newly created puppy page
+    post '/puppy' do
+        @puppy = Puppy.new(params["name"], params["breed"], params["age"])
+        erb :display_puppy
+    end
+
 end
